0297b5c63c5ea4cbd68f858bb3afa09201f8baa7
BMEgang <hgszuuob@gmail.com>
2021-11-21 22:31:18 +0000

    modify

---
+++ barcode generate/codebar/src/main/java/com/ganghu/App.java
@@ -0,0 +1,13 @@
+package com.ganghu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
